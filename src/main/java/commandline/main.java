41b80a44acf3d29b7640e7e682d312d91020e037
Evgeney Fiskin <fizpok@gmail.com>
2016-04-07 17:59:51 +0300

    TEMP 4

--- src/main/java/commandline/main.java
+++ src/main/java/commandline/main.java
@@ -6,12 +6,14 @@ import org.eclipse.jgit.api.TagCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevObject;
 import org.eclipse.jgit.revwalk.RevTag;
 import org.eclipse.jgit.transport.PushResult;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
 
@@ -27,14 +29,63 @@ public class main {
 
     static String buildNumber = "12345";
 
+    //static final String
 
     public static void main(String[] args) throws GitAPIException, IOException {
+        boolean isCorrectArgs = false;
+        int deleteCount = 0;
+        Arrays.stream(args).forEach(String::trim);
+        if (args.length < 2 || args.length > 4 || (args.length == 1 && (args[0].equals("/?") || args[0].equalsIgnoreCase("/help")))) {
+            isCorrectArgs = false;
+        } else {
+            String namePrefix = args[0].trim();
+            String buildNumber = args[1].trim();
+            if (Repository.isValidRefName(namePrefix)) {
+                if (buildNumber.isEmpty() || !isUnsignInt(buildNumber)) {
+                    isCorrectArgs = false;
+                } else {
+                    isCorrectArgs = true;
+                    String tagName = new StringBuilder(namePrefix).append('_').append(buildNumber).toString();
+                    if (args.length > 2){
+                        if(args[2].trim().equals("-d")){
+                            if (args.length == 4){
+                                if (isUnsignInt(args[3].trim())){
+                                    deleteCount=Integer.parseInt(args[3].trim());
+                                }
+                                else{
+                                    isCorrectArgs=false;
+                                }
+                            }
+                            else{
+                                deleteCount=5;
+                            }
+                        }
+                        else{
+                            isCorrectArgs=false;
+                        }
+                    } else {
+                        deleteCount = 0;
+                    }
+                }
+            }
+        }
+
+
         Git git = Core.getGit();
         Core.fetchWithTags(git);
         ttt(git);
         Collection<RevTag> allTagsWithDate = Core.getAllAnnotatedTagsByDate(git);
         String[] allTags = Core.getAllTags(git);
-   //     Core.deleteTags(git, allTags);
+        //     Core.deleteTags(git, allTags);
+    }
+
+    private static boolean isUnsignInt(String str) {
+        return str.matches("^\\d+$");
+    }
+
+    private static void printHelp() {
+        System.out.println("j2gt tag_name_prefix build_number Commit_revision [-d [n]]");
+
     }
 
 
